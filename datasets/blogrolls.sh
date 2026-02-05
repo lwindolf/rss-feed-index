04a69db0fd3c177aad74fb8c7909cc83a453d45c
Lars Windolf <lars.windolf@gmx.de>
2026-02-05 02:13:41 +0100

    New dataset blogrolls

---
+++ datasets/blogrolls.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Fetches all blogrolls from index.json and prints htmlUrl values to stdout
+while read -r blogroll; do
+    echo "===> Processing $blogroll" >&2
+    ( curl -sL "$blogroll" || true ) |\
+    ( grep "outline.*htmlUrl" || true ) |\
+    sed 's/^.*htmlUrl=.\([^"'"'"']*\).*$/\1/'
+done < <(jq -r '.blogrolls[]' "$(dirname "$0")/../index.json")
+
+echo "Done." >&2
