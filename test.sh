babbf9efa1e4944ec7ca1938688334ab41f93a67
Lars Windolf <lars.windolf@gmx.de>
2026-04-08 13:41:05 +0200

    Add livejournal OPML support

--- test.sh
+++ test.sh
@@ -14,54 +14,58 @@ fail() {
 
 echo "TC1: autodiscover feed"
 output=$( node bot/crawler.js --test https://lzone.de )
-echo "$output" | grep -q "u: 'https://lzone.de/feed/devops.xml'" || fail
+echo "$output" | grep -qF "u: 'https://lzone.de/feed/devops.xml'" || fail
 
 echo "TC2: feed type atom"
 # Reuse TC1 crawl result
-echo "$output" | grep -q "f: 'atom'" || fail
+echo "$output" | grep -qF "f: 'atom'" || fail
 
 echo "TC3: feed title"
 # Reuse TC1 crawl result
-echo "$output" | grep -q "n: 'DevOps Blog Feed'" || fail
+echo "$output" | grep -qF "n: 'DevOps Blog Feed'" || fail
 
 echo "TC4: minor flag is 3 (1=fediverse + 2=indieweb + 64=blogroll)"
 # Reuse TC1 crawl result
-echo "$output" | grep -q "M: 67" || fail
+echo "$output" | grep -qF "M: 67" || fail
 
 echo "TC5: minor flag is 16 (16=wordpress)"
 output=$( node bot/crawler.js --test https://thehoopdoctors.com/ )
-echo "$output" | grep -q "M: 16" || fail
+echo "$output" | grep -qF "M: 16" || fail
 
 echo "TC6: Cloudflare adult filter"
 output=$( node bot/crawler.js --test https://pornhub.com )
-echo "$output" | grep -q "Skipping pornhub.com - not resolvable" || fail
+echo "$output" | grep -qF "Skipping pornhub.com - not resolvable" || fail
 
 echo "TC7: Comment feeds are ignored"
 output=$( node bot/crawler.js --test https://cookiedatabase.org )
-echo "$output" | grep -q "u: 'https://cookiedatabase.org/comments/feed/'" && fail || true
+echo "$output" | grep -qF "u: 'https://cookiedatabase.org/comments/feed/'" && fail || true
 
 echo "TC8: <link rel=\"blogroll\"> discovery"
 output=$( node bot/crawler.js --test https://roytang.net )
-echo "$output" | grep -q "blogroll: 'https://roytang.net" || fail
+echo "$output" | grep -qF "blogroll: 'https://roytang.net" || fail
 
 echo "TC9: micro.blog OPML discovery"
 output=$( node bot/crawler.js --test https://john.philpin.com/ )
-echo "$output" | grep -q "blogroll: 'https://john.philpin.com/.well-known/recommendations.opml'" || fail
+echo "$output" | grep -qF "blogroll: 'https://john.philpin.com/.well-known/recommendations.opml'" || fail
 
-echo "TC10: https:// prefix is stripped from resulting URLs"
+echo "TC10: LiveJournal OPML discovery"
+output=$( node bot/crawler.js --test https://zaitcev.livejournal.com/ )
+echo "$output" | grep -qF "blogroll: 'http://www.livejournal.com/tools/opml.bml?user=" || fail
+
+echo "TC11: https:// prefix is stripped from resulting URLs"
 (
     cd $TMPDIR
     test -f index.json && rm index.json
     echo "https://lzone.de" >domains.txt
     output=$( node $DIRNAME/bot/crawler.js --add domains.txt )
-    grep -q '"lzone.de":' index.json || fail
+    grep -qF '"lzone.de":' index.json || fail
 )
 
-echo "TC11: trailing slash is stripped from URL"
+echo "TC12: trailing slash is stripped from URL"
 (
     cd $TMPDIR
     test -f index.json && rm index.json
     echo "https://lzone.de/" >domains.txt
     output=$( node $DIRNAME/bot/crawler.js --add domains.txt )
-    grep -q '"lzone.de":' index.json || fail
+    grep -qF '"lzone.de":' index.json || fail
 )
