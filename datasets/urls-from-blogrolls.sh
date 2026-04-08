d9a87964f1b6bbf284987bf4c1e3f8e3c3e50906
Lars Windolf <lars.windolf@gmx.de>
2026-04-08 13:55:55 +0200

    Also support only xmlUrl in blogroll

--- datasets/urls-from-blogrolls.sh
+++ datasets/urls-from-blogrolls.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Fetches all blogrolls from index.json and prints htmlUrl values to stdout
+# Fetches all blogrolls from index.json and prints htmlUrl (preferred) or xmlUrl values to stdout
 while read -r blogroll; do
     echo "===> Processing $blogroll" >&2
     ( curl -sL "$blogroll" || true ) |\
-    ( grep "outline.*htmlUrl" || true ) |\
-    sed 's/^.*htmlUrl=.\([^"'"'"']*\).*$/\1/'
+    ( egrep "outline.*(htmlUrl|xmlUrl)" || true ) |\
+    sed -e 's/^.*htmlUrl=.\([^"'"'"']*\).*$/\1/' \
+        -e 's/^.*xmlUrl=.\([^"'"'"']*\).*$/\1/'
 done < <(
     $(dirname "$0")/opml-all.js | jq -r ".blogrolls | keys | .[]"
 ) |\
